0d3227c41c1477519b5125d04f4ef64e49fc9d72
Ahsan <Ahsanxz36@gmail.com>
2020-12-13 17:44:47 +0500

    first commit

---
+++ bioProject/MainWindow.xaml.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace bioProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        /// <summary>
+        /// //////////////////////////////////////////
+        /// </summary>
+
+
+        public string filePath = "";
+        public string sequenceName1;
+        public string sequenceName2;
+        public int seqCount1;
+        public int seqCount2;
+        public string FirstSequence;
+        public string SecondSequence;
+
+      
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Fasta files (*.fasta)|*.fasta|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filename = openFileDialog.FileName;
+               // string name = (System.IO.Path.GetFileName(filename)); //get file name
+                string path = (Path.GetFullPath(filename));
+                fileNametxt.Content = path;
+
+                filePath = path;
+                startBtn.IsEnabled = true;
+
+
+
+            }
+        }
+
+
+        
+
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string input = "INPUT:\n######\n";
+            string sequences = File.ReadAllText(filePath);
+            string scores = "\n Scores:    match = +2, mismatch = -2, gap = -2";
+
+            // Split with multiple separators  
+            string multiCharString = sequences;
+            // Split authors separated by a comma followed by space  
+            string[] multiArray = multiCharString.Split(new Char[] { '>', '\n' });
+
+            foreach (string author in multiArray)
+            {
+                //if (author.Trim() != "")
+                  //  Debug.WriteLine(author);
+            }
+
+           
+
+            // Get Sequence Names
+
+            for (int i = 0; i < multiArray.Length; i++)
+            {
+                if (multiArray[i].Contains('1'))
+                {
+                    
+                    sequenceName1 = multiArray[i];
+                  
+                   
+                   // Debug.WriteLine(sequenceName1);
+                }
+                if (multiArray[i].Contains('2'))
+                {
+
+                    sequenceName2 = multiArray[i];
+
+
+                    //Debug.WriteLine(sequenceName2);
+                }
+            }
+
+            /// end names
+            /// 
+
+            // Get Sequences Chrachters Count
+
+            string replaceSeq = sequences;
+            string x1 = replaceSeq.Replace(sequenceName1, "");
+            string x2 = x1.Replace(sequenceName2, "");
+            string x3 = x2.Replace(sequenceName2, "");
+            string x4 = x3.Replace("\r", "");
+            string x5 = x4.Replace("\n", "");
+
+            // Split with multiple separators  
+            string changedSeq = x5;
+            // Split authors separated by a comma followed by space  
+            string[] seqArr = changedSeq.Split(new Char[] { '>' });
+
+
+
+            for (int i = 0; i < seqArr.Length; i++)
+            {
+               // Debug.WriteLine(seqArr[i]);
+            }
+
+            seqCount1 =  seqArr[1].Length;
+            seqCount2 = seqArr[2].Length;
+
+            FirstSequence = seqArr[1];
+            SecondSequence = seqArr[2];
+
+
+            //////////////////////////////////// end count
+
+
+            ////////////////////////////// Output line Editing ////////////////////////
+            string sequence1 = "\n Sequence 1 =" + sequenceName1 + "," + "Length = " + seqCount1 + "Characters";
+            string sequence2 = "\n Sequence 2 =" + sequenceName2 + "," + "Length = " + seqCount2 + "Characters";
+
+            string seq1 = sequence1;
+            string y1 = seq1.Replace("\n", "");
+            string y2 = y1.Replace("\r", "");
+
+            string seq2 = sequence2;
+            string y3 = seq2.Replace("\n", "");
+            string y4 = y3.Replace("\r", "");
+
+            string needleMan = "\n\nNeedleman-Wunsch-OUTPUT: \n#############################\n\n";
+
+            
+            ///////////////////////////////////////////
+
+            //// NeedleMan wunch
+            ///
+            NeedleAlgorithm n = new NeedleAlgorithm();
+            n.SequenceAlignmnet(FirstSequence,SecondSequence);
+
+            string aligned1 = n.align1;
+            string aligned2 = n.align2;
+
+            int matched = n.noMatches;
+            int mismatched = n.noMismatches;
+            int gapes = n.noGaps;
+            int maxLenght = n.maxIdentify;
+           // maxLenght = matched / maxLenght;
+
+            
+            string report = "\n\nReport: \n\n Alignment: Needleman-Wunsch-Algorithm-Global \n\n Number of: matches = " + matched + ", Mismatches = " + mismatched + ", gaps = " + gapes + "\n\n  Identities = " + matched + " / " + maxLenght + " , Gaps = " + gapes + " / " + maxLenght;
+           
+            ////// Water-Smith
+
+
+            string waterSmith = "\n\n Smith-Waterman-OUTPUT: \n ***************************\n\n";
+
+            smithAlgo smith = new smithAlgo();
+            smith.SequenceAlignmnet(FirstSequence, SecondSequence);
+
+            string waligned1 = smith.align1;
+            string waligned2 = smith.align2;
+
+            int wmatched = smith.noMatches;
+            int wmismatched = smith.noMismatches;
+            int wgapes = smith.noGaps;
+            int wmaxLenght = smith.maxIdentify;
+
+
+            string report2 = "\n\nReport: \n\n Alignment: Smith-Waterman-Algorithm-Local \n\n Number of: matches = " + wmatched + ", Mismatches = " + wmismatched + ", gaps = " + wgapes + "\n\n  Identities = " + wmatched + " / " + wmaxLenght + " , Gaps = " + wgapes + " / " + wmaxLenght;
+
+
+
+            outputText.Text = input + sequences + scores + "\n" + y2 + "\n" + y4 + needleMan + "s1   " + aligned1 + "\n" + "s2   " + aligned2 + report + waterSmith + "s1   " + waligned1 + "\n" + "s2   " + waligned2 + report;
+
+            // outputText.Text = File.ReadAllText(filePath);
+        }
+
+        
+    }
+}
---
+++ bioProject/NeedleAlgorithm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace bioProject
+{
+    public class NeedleAlgorithm
+    {
+
+        public string align1 = "";
+        public string align2 = "";
+
+        public int noMatches =0;
+        public int noMismatches=0;
+        public int noGaps=0;
+
+        public int maxIdentify;
+
+        public void SequenceAlignmnet(string seq1, string seq2)
+        {
+
+
+
+
+            string str1 = seq1;                       //"ACGCATCA";
+            str1.ToUpper();
+            string str2 = seq2;                            //"ACTGATTCA";
+            str2.ToUpper();
+
+            int s1Length = str1.Length +1;                  //seq1.Length;
+            int s2Length = str2.Length +1;                 //seq2.Length;
+
+
+
+            //identitfy max length
+            int max1 = str1.Length;
+            int max2 = str2.Length;
+
+            int[] maxArryx = { max1, max2 };
+            int maxIdenity = maxArryx.Max();
+            maxIdentify = maxIdenity;
+
+            char[] s1 = str1.ToCharArray();             //seq1.ToCharArray();
+            char[] s2 = str2.ToCharArray();                 //seq2.ToCharArray();
+
+            char[] arrng = { ' ' };
+            
+
+            int match = 2;
+            int mismatch = -2;
+            int gap = -1;
+
+            ///////
+            int s1gap = 0;
+            int s2gap = 0;
+            ///////
+            
+            int[,] Arr = new int[s1Length,s2Length];
+
+            for(int i =0; i<s1Length; i++)
+            {
+                int j = 0;
+                
+                Arr[i, j] = s1gap;
+                s1gap = s1gap - 2;
+
+                //Debug.Write(Arr[i,j]);
+                
+            }
+            for (int j = 0; j < s2Length; j++)
+            {
+                int i = 0;
+
+                Arr[i, j] = s2gap;
+                s2gap = s2gap - 2;
+
+                //Debug.WriteLine(Arr[i, j]);
+              
+            }
+
+            // making matrix
+
+            for(int i =1; i<s1Length; i++)
+            {
+                int toTop;
+                int toLeft;
+                int toTopleft;
+
+                for(int j =1; j<s2Length; j++)
+                {
+                    if (s1[i-1] == s2[j-1])
+                    {
+                        toTop = Arr[i, j-1];
+                        toLeft = Arr[i-1, j];
+                        toTopleft = Arr[i-1,j-1];
+
+                        toTop = toTop + gap;
+                        toLeft = toLeft + gap;
+                        toTopleft = toTopleft + match;
+
+                        int[] maxArr = { toTop, toLeft, toTopleft };
+                        int maxValue = maxArr.Max();
+                        Arr[i, j] = maxValue;
+
+                    if(i==j)
+                        {
+                            if (maxArr[0] == maxValue)
+                            {
+                                noGaps++;
+                            }
+                            if (maxArr[1] == maxValue)
+                            {
+                                noGaps++;
+                            }
+                            if (maxArr[2] == maxValue)
+                            {
+                                noMatches++;
+                            }
+                        }
+                      
+
+
+                    }
+                   else if (s1[i-1] != s2[j-1])
+                        {
+                        toTop = Arr[i, j - 1];
+                        toLeft = Arr[i - 1, j];
+                        toTopleft = Arr[i - 1, j - 1];
+
+                        toTop = toTop + gap;
+                        toLeft = toLeft + gap;
+                        toTopleft = toTopleft + mismatch;
+
+                        int[] maxArr = { toTop, toLeft, toTopleft };
+                        int maxValue = maxArr.Max();
+                        Arr[i, j] = maxValue;
+
+
+                        if (i == j)
+                        {
+                            if (maxArr[0] == maxValue)
+                            {
+                                noGaps++;
+                            }
+                            if (maxArr[1] == maxValue)
+                            {
+                                noGaps++;
+                            }
+                            if (maxArr[2] == maxValue)
+                            {
+                                noMismatches++;
+                            }
+                        }
+
+
+                    }
+
+
+
+                   
+                }
+            }
+
+
+            //Display Matrix
+            for(int i=0; i<s1Length; i++)
+            {
+                for(int j=0; j<s2Length; j++)
+                {
+                    Debug.Write(" ");
+                    
+                    Debug.Write(Arr[i,j]);
+                }
+               
+                Debug.Write("\n");
+               
+
+            }
+
+
+
+            //Sequence ALignment
+        
+            //Traceback Step
+            char[] alineSeqArray = s1;
+            char[] refSeqArray = s2;
+
+            string AlignmentA = string.Empty;
+            string AlignmentB = string.Empty;
+            int m = s1Length - 1;
+            int n = s2Length - 1;
+            while (m > 0 || n > 0)
+            {
+                int scroeDiag = 0;
+
+                if (m == 0 && n > 0)
+                {
+                    AlignmentA = refSeqArray[n - 1] + AlignmentA;
+                    AlignmentB = "-" + AlignmentB;
+                    n = n - 1;
+                }
+                else if (n == 0 && m > 0)
+                {
+                    AlignmentA = "-" + AlignmentA;
+                    AlignmentB = alineSeqArray[m - 1] + AlignmentB;
+                    m = m - 1;
+                }
+                else
+                {
+                    //Remembering that the scoring scheme is +2 for a match, -1 for a mismatch, and -2 for a gap
+                    if (alineSeqArray[m - 1] == refSeqArray[n - 1])
+                        scroeDiag = 2;
+                    else
+                        scroeDiag = -2;
+
+                    if (m > 0 && n > 0 && Arr[m, n] == Arr[m - 1, n - 1] + scroeDiag)
+                    {
+                        AlignmentA = refSeqArray[n - 1] + AlignmentA;
+                        AlignmentB = alineSeqArray[m - 1] + AlignmentB;
+                        m = m - 1;
+                        n = n - 1;
+                    }
+                    else if (n > 0 && Arr[m, n] == Arr[m, n - 1] - 2)
+                    {
+                        AlignmentA = refSeqArray[n - 1] + AlignmentA;
+                        AlignmentB = "-" + AlignmentB;
+                        n = n - 1;
+                    }
+                    else //if (m > 0 && scoringMatrix[m, n] == scoringMatrix[m - 1, n] + -2)
+                    {
+                        AlignmentA = "-" + AlignmentA;
+                        AlignmentB = alineSeqArray[m - 1] + AlignmentB;
+                        m = m - 1;
+                    }
+                }
+            }
+
+
+            align1 = AlignmentA;
+            align2 = AlignmentB;
+
+
+            Debug.Write(Environment.NewLine);
+            Debug.WriteLine(AlignmentA);
+            Debug.WriteLine(AlignmentB);
+            Debug.WriteLine("\n");
+
+
+
+
+
+
+
+        }
+
+    }
+}
